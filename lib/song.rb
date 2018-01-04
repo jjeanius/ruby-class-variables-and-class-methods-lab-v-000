133020a53bce8f9265fde8050ce1fbf7f79574bc
Judy McGoldrick <jjeanius@verizon.net>
2018-01-04 17:45:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ class Song
   @@count= 0
 
   def initialize(name, artist, genre)
-    @@name = name
-    @@artist = artist
-    @@genre = genre
-    @count +=1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
   end
 
     def self.count
